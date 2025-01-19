97c7fd9441301f69400c582b1a412c4bda7f3bf0
Carolyn <nicecarolyn@gmail.com>
2025-01-19 12:21:45 -0500

    updates

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,20 @@
-import express from 'express'
+import express, { response } from 'express'
 import db from '../dbConnections.js'
 
 const Router = express.Router()
 
 Router.get ("/", (req,res)=> {
-    db.query ("SELECT * FROM favoritecolors.users WHERE username= 'alice' AND password= 123git add", (err,result)=> {
+   const {username, password} = req.query
+   console.log(username)
+   console.log(password)
+   console.log ("haha")
+    db.query ("SELECT * FROM users WHERE username= ? AND password= ?", [username, password], (err,result)=> {
       if (err) {
         console.log ("Error in Finding User:", err)
         res.status(500).send("error in the Query")
       }   
       else
+      console.log (result)
         res.send (result)
     })
   })
---
+++ server/routes/userDashboard.jsx
@@ -0,0 +1,3 @@
+import express, { response } from 'express'
+import db from '../componenet/userDashboard.js'
+
